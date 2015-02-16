c9564263346fbbce76a0e1995b2bfac9747dac66
Cody R Fitzgerald <codypsa@macbook-pro.devices.brown.edu>
2015-02-16 17:18:30 -0500

    storyboarding

---
+++ CourseCub/CourseDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CourseDetailViewController.swift
+//  CourseCub
+//
+//  Created by Cody R Fitzgerald on 2/16/15.
+//  Copyright (c) 2015 Cody R Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class CourseDetailViewController: UIViewController  {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
