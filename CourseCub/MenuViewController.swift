84aebb5e53275b318f03f10eaaf36ef0a2798723
Alexander Meade <alec@Alexander-Meades-MacBook-Pro.local>
2015-04-25 22:23:31 -0400

    SideBar and Term Settings

---
+++ CourseCub/MenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MenuViewController.swift
+//  CourseCub
+//
+//  Created by Alexander Meade on 24/04/15.
+//  Copyright (c) 2015 Cody R Fitzgerald. All rights reserved.
+//
+
+import Foundation
