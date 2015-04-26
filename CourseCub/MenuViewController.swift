034ed782e87e3c10f1bb65a7a0b26568912e479e
Alexander Meade <alec@Alexander-Meades-MacBook-Pro.local>
2015-04-25 22:23:36 -0400

    SideBar and Term Settings

--- CourseCub/MenuViewController.swift
+++ CourseCub/MenuViewController.swift
@@ -6,4 +6,31 @@
 //  Copyright (c) 2015 Cody R Fitzgerald. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MenuViewController: UITableViewController {
+    
+    let COURSE_TERM_KEY = "COURSE_TERM"
+    let COURSE_TERM_INDEX = "COURSE_INDEX"
+    var termArray = ["Spring 2014", "Fall 2014", "Spring 2015", "Fall 2015"]
+    var storedTerm = "Spring 2015"
+    var storedIndex = 2
+    
+    @IBOutlet weak var termPicker: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- CourseCub/SettingsViewController.swift
+++ CourseCub/SettingsViewController.swift
@@ -6,4 +6,66 @@
 //  Copyright (c) 2015 Cody R Fitzgerald. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var termPicker: UIPickerView!
+    
+    var termArray = ["Spring 2014", "Fall 2014", "Spring 2015", "Fall 2015"]
+    var termCode = ["201420", "201410", "201520", "201510"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        var tempTerm: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey(appDelegate.COURSE_TERM_KEY)
+        var tempIndex: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey(appDelegate.COURSE_TERM_INDEX)
+        var tempCode: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey(appDelegate.COURSE_TERM_CODE)
+    
+        
+        self.termPicker.dataSource = self
+        self.termPicker.delegate = self
+        self.termPicker.selectRow(tempIndex as! Int, inComponent: 0, animated: true)
+    
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return termArray.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return termArray[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(termArray[row], forKey: appDelegate.COURSE_TERM_KEY)
+        defaults.setObject(row, forKey: appDelegate.COURSE_TERM_INDEX)
+        defaults.setObject(termCode[row], forKey: appDelegate.COURSE_TERM_CODE)
+        defaults.synchronize()
+    }
+    
+}
